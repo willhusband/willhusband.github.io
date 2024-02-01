5a523027dded1954ff2709cf917cbffcf60c16d0
willhusband <will.husband.2106@gmail.com>
2024-02-01 14:08:34 +0000

    new button js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function scrollToContact() {
     document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
 }
+
+        document.getElementById('contact-btn').addEventListener('click', function() {
+            document.querySelector('footer').scrollIntoView({ 
+                behavior: 'smooth' 
+            });
+        });
